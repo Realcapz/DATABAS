e4da408c519dbf7da70f8b3e036e2a70e876b76c
Realcapz <144321466+Realcapz@users.noreply.github.com>
2023-12-07 12:56:28 +0100

    Ahmed API

---
+++ index.js
@@ -0,0 +1,115 @@
+let express = require("express");
+let mysql = require("mysql");
+let crypto = require("crypto");
+
+let app = express();
+let PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.use(express.json());
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "jensen2023",
+  multipleStatements: true,
+});
+
+let COLUMNS = ["id", "username", "password", "name", "email"];
+
+// Serve your documentation
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.get("/users", function (req, res) {
+  let sql = "SELECT * FROM users"; 
+  let condition = createCondition(req.query); 
+  console.log(sql + condition); 
+  con.query(sql + condition, function (err, result, fields) {
+    res.send(result);
+  });
+});
+
+// Function to hash data
+function hash(data) {
+  let hash = crypto.createHash("sha256");
+  hash.update(data);
+  return hash.digest("hex");
+}
+
+// Create condition for SQL queries
+function createCondition(query) {
+  let output = " WHERE ";
+  for (let key in query) {
+    if (COLUMNS.includes(key)) {
+      output += `${key}="${query[key]}" OR `;
+    }
+  }
+  if (output.length === 7) {
+    return "";
+  } else {
+    return output.substring(0, output.length - 4);
+  }
+}
+
+// Route to add a new user with hashed password
+app.post("/users", (req, res) => {
+  if (!req.body.username) {
+    res.status(400).send("username required!");
+    return;
+  }
+  
+
+  let fields = ["name", "password", "email", "username"];
+  for (let key in req.body) {
+    if (!fields.includes(key)) {
+      res.status(400).send("Unknown field: " + key);
+      return;
+    }
+  }
+
+  let sql = `INSERT INTO users (username, email, name, password) 
+  VALUES ('${req.body.username}', 
+          '${req.body.email}',
+          '${req.body.name}',
+          '${hash(req.body.password)}');
+          SELECT LAST_INSERT_ID();`;
+
+  console.log(sql);
+
+  con.query(sql, (err, result, fields) => {
+    if (err) {
+  
+    res.status(500).send("Error inserting data");
+    throw err;
+}
+    console.log(result);
+    let output = {
+      id: result[0].insertId,
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+    };
+    res.send(output);
+  });
+});
+
+app.get("/users/:id", function (req, res) {
+  // Värdet på id ligger i req.params
+  let sql = "SELECT * FROM users WHERE id=" + req.params.id;
+  console.log(sql);
+  // skicka query till databasen
+  con.query(sql, function (err, result, fields) {
+    if (result.length > 0) {
+      res.send(result);
+    } else {
+      res.sendStatus(404); // 404=not found
+    }
+  });
+});
---
+++ users.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 07, 2023 at 12:09 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jensen2023`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`) VALUES
+(1, 'kalleanka', 'mypassword', 'Kalle Anka', 'kalle@anka.se'),
+(2, 'b', '99ae59705a7898c8c9b30b1ad4be3566212e3121c0ca5a1224f04f61fbaa72b2', 'BERTIL', 'Bertil@p.se'),
+(4, 'ahmed1', '99ae59705a7898c8c9b30b1ad4be3566212e3121c0ca5a1224f04f61fbaa72b2', 'BERTIL', 'Bertil@p.se');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
